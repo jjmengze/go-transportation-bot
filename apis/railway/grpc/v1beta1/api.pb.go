2b85f084c53a77d5ffd4672d83511ee45482d2a9
MengZn <adnt587@gmail.com>
2020-10-05 11:39:08 +0800

    Update grpc api for railway query city

--- apis/railway/grpc/v1beta1/api.pb.go
+++ apis/railway/grpc/v1beta1/api.pb.go
@@ -7,7 +7,11 @@
 package v1beta1
 
 import (
+	context "context"
 	proto "github.com/golang/protobuf/proto"
+	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
@@ -175,13 +179,13 @@ var file_api_proto_rawDesc = []byte{
 	0x62, 0x65, 0x74, 0x61, 0x31, 0x2e, 0x43, 0x69, 0x74, 0x79, 0x52, 0x04, 0x63, 0x69, 0x74, 0x79,
 	0x22, 0x2a, 0x0a, 0x04, 0x43, 0x69, 0x74, 0x79, 0x12, 0x0e, 0x0a, 0x02, 0x49, 0x44, 0x18, 0x01,
 	0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x49, 0x44, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65,
-	0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x32, 0x3a, 0x0a, 0x07,
-	0x52, 0x61, 0x69, 0x6c, 0x77, 0x61, 0x79, 0x12, 0x2f, 0x0a, 0x04, 0x43, 0x69, 0x74, 0x79, 0x12,
-	0x0e, 0x2e, 0x76, 0x31, 0x62, 0x65, 0x74, 0x61, 0x31, 0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x1a,
-	0x15, 0x2e, 0x76, 0x31, 0x62, 0x65, 0x74, 0x61, 0x31, 0x2e, 0x43, 0x69, 0x74, 0x79, 0x52, 0x65,
-	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x1b, 0x5a, 0x19, 0x61, 0x70, 0x69, 0x73,
-	0x2f, 0x72, 0x61, 0x69, 0x6c, 0x77, 0x61, 0x79, 0x2f, 0x67, 0x72, 0x70, 0x63, 0x2f, 0x76, 0x31,
-	0x62, 0x65, 0x74, 0x61, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x32, 0x3d, 0x0a, 0x07,
+	0x52, 0x61, 0x69, 0x6c, 0x77, 0x61, 0x79, 0x12, 0x32, 0x0a, 0x07, 0x47, 0x65, 0x74, 0x43, 0x69,
+	0x74, 0x79, 0x12, 0x0e, 0x2e, 0x76, 0x31, 0x62, 0x65, 0x74, 0x61, 0x31, 0x2e, 0x45, 0x6d, 0x70,
+	0x74, 0x79, 0x1a, 0x15, 0x2e, 0x76, 0x31, 0x62, 0x65, 0x74, 0x61, 0x31, 0x2e, 0x43, 0x69, 0x74,
+	0x79, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x1b, 0x5a, 0x19, 0x61,
+	0x70, 0x69, 0x73, 0x2f, 0x72, 0x61, 0x69, 0x6c, 0x77, 0x61, 0x79, 0x2f, 0x67, 0x72, 0x70, 0x63,
+	0x2f, 0x76, 0x31, 0x62, 0x65, 0x74, 0x61, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -204,8 +208,8 @@ var file_api_proto_goTypes = []interface{}{
 }
 var file_api_proto_depIdxs = []int32{
 	2, // 0: v1beta1.CityResponse.city:type_name -> v1beta1.City
-	0, // 1: v1beta1.Railway.City:input_type -> v1beta1.Empty
-	1, // 2: v1beta1.Railway.City:output_type -> v1beta1.CityResponse
+	0, // 1: v1beta1.Railway.GetCity:input_type -> v1beta1.Empty
+	1, // 2: v1beta1.Railway.GetCity:output_type -> v1beta1.CityResponse
 	2, // [2:3] is the sub-list for method output_type
 	1, // [1:2] is the sub-list for method input_type
 	1, // [1:1] is the sub-list for extension type_name
@@ -274,4 +278,84 @@ func file_api_proto_init() {
 	file_api_proto_rawDesc = nil
 	file_api_proto_goTypes = nil
 	file_api_proto_depIdxs = nil
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConnInterface
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion6
+
+// RailwayClient is the client API for Railway service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type RailwayClient interface {
+	GetCity(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*CityResponse, error)
+}
+
+type railwayClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewRailwayClient(cc grpc.ClientConnInterface) RailwayClient {
+	return &railwayClient{cc}
+}
+
+func (c *railwayClient) GetCity(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*CityResponse, error) {
+	out := new(CityResponse)
+	err := c.cc.Invoke(ctx, "/v1beta1.Railway/GetCity", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// RailwayServer is the server API for Railway service.
+type RailwayServer interface {
+	GetCity(context.Context, *Empty) (*CityResponse, error)
+}
+
+// UnimplementedRailwayServer can be embedded to have forward compatible implementations.
+type UnimplementedRailwayServer struct {
+}
+
+func (*UnimplementedRailwayServer) GetCity(context.Context, *Empty) (*CityResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetCity not implemented")
+}
+
+func RegisterRailwayServer(s *grpc.Server, srv RailwayServer) {
+	s.RegisterService(&_Railway_serviceDesc, srv)
+}
+
+func _Railway_GetCity_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(RailwayServer).GetCity(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/v1beta1.Railway/GetCity",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(RailwayServer).GetCity(ctx, req.(*Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _Railway_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "v1beta1.Railway",
+	HandlerType: (*RailwayServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "GetCity",
+			Handler:    _Railway_GetCity_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "api.proto",
 }
